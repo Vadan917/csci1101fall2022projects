cdbea81ca0cea04f6e9ce7db7815df468c8bad2f
VA <valnor12@hotmail.com>
2022-11-21 13:35:59 -0600

    crud-operations

--- games-data/sandbox-crud-operations-query.sql
+++ games-data/sandbox-crud-operations-query.sql
@@ -1,6 +1,6 @@
 SELECT * FROM sandbox;
 
---INSERT INTO sandbox(int64_value, string_value, int16_value) VALUES (134679, 'onepiece', 31999);
+--INSERT INTO sandbox(int64_value, string_value, int16_value) VALUES (813164054, 'the game', 8731);
 
 --UPDATE sandbox SET string_value = 'the game' WHERE id = 1;
 
