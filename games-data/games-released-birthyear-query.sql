4fb8299929164652b4683bee80ab5093e2e03ce8
VA <valnor12@hotmail.com>
2022-11-21 13:31:10 -0600

    Add games_data_project

---
+++ games-data/games-released-birthyear-query.sql
@@ -0,0 +1,1 @@
+SELECT * FROM games WHERE first_release_date BETWEEN '1999-01-01 00:00:00' and '1999-12-31 23:59:59' ORDER BY name;
---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int64_value, string_value, int16_value) VALUES (134679, 'onepiece', 31999);
+
+--UPDATE sandbox SET string_value = 'the game' WHERE id = 1;
+
+--DELETE FROM sandbox WHERE id = 8;
